e9756e6bc073719779c194a6960fce25ee7e3bdf
JeelHirani006 <24010101101@darshan.ac.in>
2025-01-20 19:40:58 +0530

    new folder

---
+++ class-java/ch-2/class/Demo1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ class Box
+  {
+  	 int length;
+      int hight;
+  }
+public class Demo1{	
+	public static void main (String [] args)
+	{
+         Box a=new Box();
+         Box b=new Box();
+         a.length=10;
+         a.hight=10;
+         b.length=20;
+         b.hight=20;
+         System.out.println(a.length);
+	}
+}
+	
